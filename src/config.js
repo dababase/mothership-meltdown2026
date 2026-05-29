11dc68970231fb3751842b009ce0491827d7dfe2
nicksomething-lang <nickesilberman@gmail.com>
2026-05-28 16:06:38 -1000

    Update config.js talk w kyle

--- src/config.js
+++ src/config.js
@@ -1,38 +1,35 @@
-export const EVENT_CODE = 'MM26';
-export const EVENT_NAME = 'Mothership Meltdown 2026';
+export const EVENT_CODE = 'MM26'
+export const EVENT_NAME = 'Mothership Meltdown 2026'
 
 export const CATEGORIES = [
   {
     key: 'appearance',
     label: 'Appearance',
-    maxScore: 20,
+    maxScore: 100,
     description: 'Consider color and consistency. Is the oil wet or dry?',
   },
   {
     key: 'aroma',
     label: 'Aroma',
-    maxScore: 20,
-    description:
-      'Are the terpenes pronounced or muted? Do you detect any degradation or off-notes? Does the aroma have depth, or does it fall flat? How does it make you feel?',
+    maxScore: 100,
+    description: 'Are the terpenes pronounced or muted? Do you detect any degradation or off-notes? Does the aroma have depth, or does it fall flat? How does it make you feel?',
   },
   {
     key: 'taste',
     label: 'Taste',
-    maxScore: 20,
-    description:
-      'How does the extract coat your palate? Is the flavor long-lasting? Does it stand out as exotic, or is it more standard?',
+    maxScore: 100,
+    description: 'How does the extract coat your palate? Is the flavor long-lasting? Does it stand out as exotic, or is it more standard?',
   },
   {
     key: 'melt_quality',
     label: 'Melt Quality',
-    maxScore: 20,
-    description:
-      'Does it provide a clean melt? Does it behave like water, or are there contaminants or lower-melting terpenes causing residue? How does the hit feel on your throat and lungs?',
+    maxScore: 100,
+    description: 'Does it provide a clean melt? Does it behave like water, or are there contaminants or lower-melting terpenes causing residue? How does the hit feel on your throat and lungs?',
   },
   {
     key: 'experience',
     label: 'Experience',
-    maxScore: 20,
+    maxScore: 100,
     description: 'How is the high? How is the lasting taste on your palate?',
   },
-];
+]
